fab53b78a04864e3ce7db6264361661ad5110fa6
JesusPieceHoly <116229206+JesusPieceHoly@users.noreply.github.com>
2024-12-23 02:48:33 -0500

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,6 @@ import { Fragment } from "react";
 import { usePathname } from "next/navigation";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import Image from "next/image";
-import ThemeSwitcher from './ThemeSwitcher';
 
 const navigation = [
   { name: "Tokens", href: "/" },
@@ -60,7 +59,6 @@ export default function Navbar({}: {}) {
                 </div>
               </div>
               <div className="hidden sm:ml-6 sm:flex sm:items-center">
-                <ThemeSwitcher /> {/* Adding ThemeSwitcher here */}
                 <Menu as="div" className="relative ml-3">
                   <div></div>
                   <Transition
@@ -78,43 +76,4 @@ export default function Navbar({}: {}) {
               </div>
               <div className="-mr-2 flex items-center sm:hidden">
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">
-                  <span className="sr-only">Open main menu</span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 50 50"
-                    width="24px"
-                    height="24px"
-                  >
-                    <path d="M 5 8 A 2.0002 2.0002 0 1 0 5 12 L 45 12 A 2.0002 2.0002 0 1 0 45 8 L 5 8 z M 5 23 A 2.0002 2.0002 0 1 0 5 27 L 45 27 A 2.0002 2.0002 0 1 0 45 23 L 5 23 z M 5 38 A 2.0002 2.0002 0 1 0 5 42 L 45 42 A 2.0002 2.0002 0 1 0 45 38 L 5 38 z" />
-                  </svg>
-                </Disclosure.Button>
-              </div>
-            </div>
-          </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 pt-2 pb-3">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    pathname === item.href
-                      ? "bg-slate-50 border-slate-500 text-slate-700"
-                      : "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800",
-                    "block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-                  )}
-                  aria-current={pathname === item.href ? "page" : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-            <div className="border-t border-gray-200 pt-4 pb-3"></div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
-  );
-}
+                  <span className
