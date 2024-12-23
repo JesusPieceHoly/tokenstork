ea24dcc9743dffe6742b38bea580786c04ef4a0f
JesusPieceHoly <116229206+JesusPieceHoly@users.noreply.github.com>
2024-12-23 10:17:39 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import type { Metadata } from "next";
 import HelloBar from "@/app/components/HelloBar";
 import Footer from "@/app/components/Footer";
@@ -7,6 +9,7 @@ import GoogleAnalytics from "@/app/components/GoogleAnalytics";
 import ThemeSwitcher from "@/app/components/ThemeSwitcher"; // Import ThemeSwitcher
 import { BCHPriceProvider } from "@/app/providers/bchpriceclientprovider";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from 'next-themes';
 import "@/app/globals.css"; // Ensure correct path to globals.css
 
 const bodyFont = Inter({
@@ -62,25 +65,27 @@ export default function RootLayout({
   tokenId?: string;
 }) {
   return (
-    <BCHPriceProvider>
-      <html lang="en" className={`${bodyFont.variable} font-sans antialiased dark:bg-gray-950`}>
-        <head>
-          <script
-            src="https://beamanalytics.b-cdn.net/beam.min.js"
-            data-token="c989accf-6494-49a8-ad3a-ee34c91aeedd"
-            async
-          ></script>
-        </head>
-        <body className="container mx-auto">
-          <HelloBar />
-          <Navbar />
-          {children}
-          <CTA />
-          <Footer />
-          <GoogleAnalytics />
-          <ThemeSwitcher /> {/* Add ThemeSwitcher as a floating button */}
-        </body>
-      </html>
-    </BCHPriceProvider>
+    <ThemeProvider attribute="class">
+      <BCHPriceProvider>
+        <html lang="en" className={`${bodyFont.variable} font-sans antialiased dark:bg-gray-950`}>
+          <head>
+            <script
+              src="https://beamanalytics.b-cdn.net/beam.min.js"
+              data-token="c989accf-6494-49a8-ad3a-ee34c91aeedd"
+              async
+            ></script>
+          </head>
+          <body className="container mx-auto">
+            <HelloBar />
+            <Navbar />
+            {children}
+            <CTA />
+            <Footer />
+            <GoogleAnalytics />
+            <ThemeSwitcher /> {/* Add ThemeSwitcher as a floating button */}
+          </body>
+        </html>
+      </BCHPriceProvider>
+    </ThemeProvider>
   );
 }
