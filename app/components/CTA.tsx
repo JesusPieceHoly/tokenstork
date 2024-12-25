8fb1d4388b2643f0ed4f92425188a03c88586bf3
JesusPieceHoly <116229206+JesusPieceHoly@users.noreply.github.com>
2024-12-25 12:02:05 -0500

    Update CTA.tsx

--- app/components/CTA.tsx
+++ app/components/CTA.tsx
@@ -1,13 +1,11 @@
-// TODO: make it into a grid and on top of the CTA put 2 items that are ads perhaps.
-
 const CTA = () => {
   return (
     <section className="mt-8 bg-accent hover:bg-primary rounded-lg">
       <div className="flex flex-col items-center justify-center p-4 space-y-8 md:p-10 lg:space-y-0 lg:flex-row lg:justify-between">
-        <h3 className="text-3xl font-semibold leadi text-center lg:text-left text-white">
+        <h3 className="text-3xl font-semibold leading text-center lg:text-left text-purple-500">
           Cash Tokens Telegram Channel - Join the Conversation!
         </h3>
-        <button className="px-8 py-3 text-lg font-semibold rounded bg-white">
+        <button className="px-8 py-3 text-lg font-semibold rounded bg-white text-black"> {/* Added text-black here */}
           <a
             className="no-underline"
             href="https://t.me/CashTokensBitcoinCash"
